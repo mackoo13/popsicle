5a0ad1ce3316396b1ae03b67f3ae640c2e8ddb99
mkocot <kocot@student.agh.edu.pl>
2018-07-05 16:40:01 +0200

    listing available events

--- events.c
+++ events.c
@@ -6,7 +6,23 @@ void initialize() {
     if(!PAPI_is_initialized()) PAPI_library_init(PAPI_VER_CURRENT);
 }
 
-int main() {
+int* available_event_codes(int* res, int* number) {
+    int i = PAPI_PRESET_MASK;
+    PAPI_event_info_t info;
+
+    *number = 0;
+    initialize();
+
+    do {
+        if ((PAPI_get_event_info(i, &info) == PAPI_OK) && (PAPI_query_event(i) == PAPI_OK)) {
+            res[*number] = info.event_code;
+            *number = *number + 1;
+
+        }
+    } while (PAPI_enum_event(&i, PAPI_ENUM_ALL) == PAPI_OK);
+}
+
+int print_events_list() {
     int retval, is_available;
     int available_count = 0;
     int i = PAPI_PRESET_MASK;
@@ -30,5 +46,18 @@ int main() {
     } while (PAPI_enum_event(&i, PAPI_ENUM_ALL) == PAPI_OK);
 
     printf("\nAvailable events: %d\n", available_count);
+}
 
+int main() {
+//    print_events_list();
+
+    int res[256];
+    char EventCodeStr[PAPI_MAX_STR_LEN];
+    int number;
+    available_event_codes(res, &number);
+
+    for (int i = 0; i < number; ++i) {
+        PAPI_event_code_to_name(res[i], EventCodeStr);
+        printf("%s\n", EventCodeStr);
+    }
 }
