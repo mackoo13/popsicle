36d08e7cdba278e2a9b945cd70e6a4c10fb1ac05
mkocot <kocot@student.agh.edu.pl>
2018-07-05 10:17:02 +0200

    gcc

--- ex2.c
+++ ex2.c
@@ -1,9 +1,8 @@
 #include <papi.h>
-#include <testlib/do_loops.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_FLOPS 10000
+//#define NUM_FLOPS 10000
 #define NUM_EVENTS 1
 
 void handle_error (int retval)
@@ -21,23 +20,16 @@ int main()
     if (PAPI_start_counters(Events, NUM_EVENTS) != PAPI_OK)
         handle_error(1);
 
-/* Defined in tests/do_loops.c in the PAPI source distribution */
-    do_flops(NUM_FLOPS);
-
 /* Read the counters */
     if (PAPI_read_counters(values, NUM_EVENTS) != PAPI_OK)
         handle_error(1);
 
     printf("After reading the counters: %lld\n",values[0]);
 
-    do_flops(NUM_FLOPS);
-
 /* Add the counters */
     if (PAPI_accum_counters(values, NUM_EVENTS) != PAPI_OK)
         handle_error(1);
     printf("After adding the counters: %lld\n", values[0]);
-
-    do_flops(NUM_FLOPS);
 
 /* Stop counting events */
     if (PAPI_stop_counters(values, NUM_EVENTS) != PAPI_OK)
@@ -46,4 +38,7 @@ int main()
     printf("After stopping the counters: %lld\n", values[0]);
 }
 
-// gcc -I ~/papi/papi/src ex2.c ~/papi/papi/src/testlib/do_loops.c -L ~/papi/papi/src -lpapi
+// gcc -I ~/papi/papi/src/ -c ex2.c
+// gcc ex2.o -L ~/papi/papi/src/libpfm4/lib -lpfm  -L ~/papi/papi/src/ -lpapi -static -o ex2
+
+// gcc ex2.c -I ~/papi/papi/src/ -L ~/papi/papi/src/libpfm4/lib -lpfm  -L ~/papi/papi/src/ -lpapi -static -o ex2
