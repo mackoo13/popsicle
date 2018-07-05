3141e4543f9fd7a5ef1c857d15ae0990f97760a4
mkocot <kocot@student.agh.edu.pl>
2018-07-05 14:05:43 +0200

    stuff

--- ex2.c
+++ ex2.c
@@ -10,7 +10,25 @@ void handle_error (int retval)
     exit(1);
 }
 
-int ex(int retval) {
+void initialize() {
+    if(!PAPI_is_initialized()) PAPI_library_init(PAPI_VER_CURRENT);
+}
+
+void list_components() {
+    int num_comp = PAPI_num_components();
+    printf("Components count: %d\n", num_comp);
+
+    const PAPI_component_info_t *cmpinfo = NULL;
+    for (int i = 0; i < num_comp; ++i) {
+        cmpinfo = PAPI_get_component_info(i);
+        printf("\t[%d] %s\n", i, cmpinfo->name);
+        printf("\t\tNative events: %d\n", cmpinfo->num_native_events);
+        printf("\t\tPreset Events: %d\n", cmpinfo->num_preset_events);
+        if(cmpinfo->disabled) printf("\t\tWARNING! Component disabled - reason: %s\n", cmpinfo->disabled_reason);
+    }
+}
+
+int exec(int retval) {
     if (retval != PAPI_OK) handle_error(retval);
     return retval;
 }
@@ -21,57 +39,24 @@ int main()
     long_long values[NUM_EVENTS];
     int retval;
 
+    initialize();
+    list_components();
 
-    // Init
-    if(!PAPI_is_initialized()) PAPI_library_init(PAPI_VER_CURRENT);
+    printf("Counters count: %d\n", PAPI_num_counters());
 
-    // Event set
-    int EventSet = PAPI_NULL;
-    ex(PAPI_create_eventset(&EventSet));
-//    ex(PAPI_add_event(EventSet, PAPI_L1_DCM));
+    // Start counting events
+    exec(PAPI_start_counters(Events, NUM_EVENTS));
 
-
-    // Components
-    int num_comp = PAPI_num_components();
-    const PAPI_component_info_t *cmpinfo = NULL;
-
-    printf("Components: %d\n", num_comp);
-    for (int i = 0; i < num_comp; ++i) {
-        cmpinfo = PAPI_get_component_info(i);
-        printf("%s\n", cmpinfo->name);
-        printf("\tNative events: %d\n", cmpinfo->num_native_events);
-        printf("\tPreset Events: %d\n", cmpinfo->num_preset_events);
-        printf("\tDisabled: %s\n", cmpinfo->disabled_reason);
-    }
-
-
-    printf("Counters: %d\n", PAPI_num_counters());
-
-
-
-/* Start counting events */
-    retval = PAPI_start_counters(Events, NUM_EVENTS);
-    printf("Start: %d\n", retval);
-    if (retval != PAPI_OK)
-        handle_error(retval);
-
-/* Read the counters */
-    retval = PAPI_read_counters(values, NUM_EVENTS);
-    printf("%d\n", retval);
-    if (retval != PAPI_OK)
-        handle_error(retval);
-
+    // Read the counters
+    exec(PAPI_read_counters(values, NUM_EVENTS));
     printf("After reading the counters: %lld\n",values[0]);
 
-/* Add the counters */
-    if (PAPI_accum_counters(values, NUM_EVENTS) != PAPI_OK)
-        handle_error(103);
+    // Add the counters
+    exec(PAPI_accum_counters(values, NUM_EVENTS));
     printf("After adding the counters: %lld\n", values[0]);
 
-/* Stop counting events */
-    if (PAPI_stop_counters(values, NUM_EVENTS) != PAPI_OK)
-        handle_error(104);
-
+    // Stop counting events
+    exec(PAPI_stop_counters(values, NUM_EVENTS));
     printf("After stopping the counters: %lld\n", values[0]);
 }
 
